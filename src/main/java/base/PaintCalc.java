81ae8f7a0faa6b24113a30f5ff596bc37962db86
darkstar100x <darkstar100x@gmail.com>
2021-06-06 18:12:42 -0400

    Third Commit (Final)

--- src/main/java/base/PaintCalc.java
+++ src/main/java/base/PaintCalc.java
@@ -1,14 +1,18 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Harding
+ */
+
 package base;
 
 import java.util.Scanner;
 
 public class PaintCalc {
   Scanner input = new Scanner(System.in);
-  public final int conversion = 350;
+  final int conversion = 350;
 
   public static void main(String[] args) {
     PaintCalc paint = new PaintCalc();
-
     int area = paint.getArea(paint.getLength(), paint.getWidth());
 
     System.out.printf("You will need %d gallons of paint to cover %d square feet.%n", paint.getGallons(area), area);
@@ -16,12 +20,28 @@ public class PaintCalc {
 
   public int getLength(){
     System.out.print("What is the length of the room you wish to paint? ");
-    return input.nextInt();
+    String n = input.next();
+    try {
+      Integer.parseInt(n);
+    }
+    catch (NumberFormatException e) {
+      System.out.println("ERROR: Invalid Value! Try Again!");
+      return getLength();
+    }
+    return Integer.parseInt(n);
   }
 
   public int getWidth(){
     System.out.print("What is the width? ");
-    return input.nextInt();
+    String n = input.next();
+    try {
+      Integer.parseInt(n);
+    }
+    catch (NumberFormatException e) {
+      System.out.println("ERROR: Invalid Value! Try Again!");
+      return getWidth();
+    }
+    return Integer.parseInt(n);
   }
 
   public int getArea(int length, int width){
